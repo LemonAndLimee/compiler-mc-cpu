513ef9435bcc94e98dca30aca02de6029cab8f4c
LemonAndLimee <brockweir75@gmail.com>
2025-07-26 15:23:49 +0100

    Add Makefile and basic build support for OS X

--- Compiler/TokenValue.h
+++ Compiler/TokenValue.h
@@ -5,6 +5,10 @@
 #pragma once
 #include <memory>
 
+#ifdef OS_X
+    #include <string.h>
+#endif
+
 // Max length that a string value held by the token can be.
 constexpr size_t g_tokenStrValueMaxLen{ 32u };
 
@@ -44,7 +48,11 @@ struct TokenValue
         {
             printf( "Warning: Token value string %s exceeds max character length.\n", stringValue.c_str() );
         }
+#ifdef OS_X
+        strlcpy( m_value.stringValue, stringValue.c_str(), g_tokenStrValueMaxLen );
+#else
         strcpy_s( m_value.stringValue, g_tokenStrValueMaxLen, stringValue.c_str() );
+#endif
     };
     TokenValue( DataType dataTypeValue )
     : m_valueType( TokenValueType::DTYPE )
