9db102bf888955cb6c7f09a9a0c0d124338d1702
LemonAndLimee <brockweir75@gmail.com>
2025-07-27 13:47:48 +0200

    Add logic for checking for leftover tokens, and stub method for creating node from children

--- Compiler/AstGenerator.cpp
+++ Compiler/AstGenerator.cpp
@@ -5,8 +5,6 @@
 #include "AstGenerator.h"
 #include <stdexcept>
 
-using namespace GrammarRules;
-
 /**
  * \brief Generates an Abstract Syntax tree from a given set of tokens. If the syntax of the tokens
  *        is invalid, it throws an error.
@@ -19,7 +17,11 @@ using namespace GrammarRules;
  * \return  Pointer to the root of the generated tree.
  */
 AstNode::Ptr
-AstGenerator::GenerateAst( TokensVector& tokens, GrammarSymbols::NT startingNt, bool allowLeftoverTokens )
+AstGenerator::GenerateAst(
+    const TokensVector& tokens,
+    GrammarSymbols::NT startingNt,
+    bool allowLeftoverTokens
+)
 {
     if ( 0u == g_nonTerminalRuleSets.count( startingNt ) )
     {
@@ -35,34 +37,58 @@ AstGenerator::GenerateAst( TokensVector& tokens, GrammarSymbols::NT startingNt, 
         // Index of the current token we are consuming from the input vector
         size_t currentTokenIndex{ 0u };
 
+        // Make an editable copy of the input tokens vector
+        TokensVector tokensCopy = tokens;
+
         // If rule doesn't match tokens list, ignore and continue
-        if ( !TryRule( tokens, currentRule, children, currentTokenIndex ) )
+        if ( !TryRule( tokensCopy, currentRule, children, currentTokenIndex ) )
         {
             continue;
         }
 
         // If this point is reached, the current rule matches the list of tokens
-        // TODO: check for leftover tokens and throw if not allowed
-        // Construct an AST node from the children
+
+        // If there are leftover tokens and this is not allowed, consider this a non-match for this rule and continue
+        // looping through further rules.
+        if ( !allowLeftoverTokens )
+        {
+            if ( !tokensCopy.empty() )
+            {
+                // TODO: add warning
+                continue;
+            }
+        }
+        
+        if ( children.empty() )
+        {
+            throw std::runtime_error( "Rule match found but no child nodes or tokens created." );
+        }
+
+        // Construct an AST node from children
+        return CreateNodeFromChildren( children );
     }
 
-    throw std::runtime_error( "Not implemented.");
+    // If the loop is exited and no rule match has been found
+    // TODO: make string form of symbol names and rules for better error messages
+    throw std::runtime_error( "No matching rule could be found." );
 }
 
 /**
- * \brief Tries to resolve a given rule (collection of symbols) from the given list of tokens.
+ * \brief  Tries to resolve a given rule (collection of symbols) from the given list of tokens.
  *
- * \param[in]   tokens      String of tokens representing the code to be converted.
- * \param[in]   rule        The current rule that is being tested. Consists of symbols, either terminal or non-terminal.
- * \param[out]  children    Child nodes or tokens belonging to the rule being tested.
- * \param[out]  tokenIndex  The index of the next token after this rule has been resolved. Used by a calling function
- *                          to test a later rule, or determine if there are leftover tokens.  
+ * \param[in,out]  tokens      String of tokens representing the code to be converted. If a token matches a symbol
+ *                             in the rule, it is popped from the start of the vector.
+ * \param[in]      rule        The current rule that is being tested. Consists of symbols, either terminal or
+ *                             non-terminal.
+ * \param[out]     children    Child nodes or tokens belonging to the rule being tested.
+ * \param[out]     tokenIndex  The index of the next token after this rule has been resolved. Used by a calling
+ *                             method to test a later rule, or determine if there are leftover tokens.  
  *
  * \return  True if the rule could successfully be matched to the tokens, false otherwise.
  */
 bool
 AstGenerator::TryRule(
-    const TokensVector& tokens,
+    TokensVector& tokens,
     const Rule& rule,
     std::vector< AstNode::Child >& children,
     size_t& tokenIndex
@@ -78,4 +104,20 @@ AstGenerator::TryRule(
         // Then add this to children
         // TODO: do we need the get AST node method to update our token index i.e. taking it as out param?
     }
+}
+
+/**
+ * \brief  Creates an AST node instance from a given set of child nodes/tokens. Skips tokens that can be skipped,
+ *         e.g. punctuation, and assigns the node label according to the set of children.
+ *
+ * \param[in]  children  Child nodes or tokens belonging to the node being created.
+ *
+ * \return  Ptr to the created AST node.
+ */
+AstNode::Ptr
+AstGenerator::CreateNodeFromChildren(
+    const std::vector< AstNode::Child >& children
+)
+{
+    throw std::runtime_error( "Not implemented" );
 }
--- Compiler/AstGenerator.h
+++ Compiler/AstGenerator.h
@@ -7,6 +7,7 @@
 #include "AstNode.h"
 
 using TokensVector = std::vector< Token::Ptr >;
+using namespace GrammarRules;
 
 class AstGenerator
 {
@@ -14,8 +15,10 @@ public:
     using Ptr = std::shared_ptr< AstGenerator >;
     AstGenerator() = default;
 
-    AstNode::Ptr GenerateAst( TokensVector& tokens, GrammarSymbols::NT startingNt, bool allowLeftoverTokens );
+    AstNode::Ptr GenerateAst( const TokensVector& tokens, GrammarSymbols::NT startingNt, bool allowLeftoverTokens );
 
 private:
-    bool TryRule( const TokensVector& tokens, const Rule& rule, std::vector< AstNode::Child >& children, size_t& tokenIndex );
+    bool TryRule( TokensVector& tokens, const Rule& rule, std::vector< AstNode::Child >& children, size_t& tokenIndex );
+
+    AstNode::Ptr CreateNodeFromChildren( const std::vector< AstNode::Child >& children );
 };
